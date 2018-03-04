7a2d602c50eaaa26136c422135049f2aa4b130a6
unknown <adrianomulinari@hotmail.com>
2018-03-03 21:02:27 -0300

    first commit

---
+++ src/main/java/com/mulinari/spreadpage/SpreadPageController.java
@@ -0,0 +1,17 @@
+package com.mulinari.spreadpage;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/{userId}")
+public class SpreadPageController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	String getMethod(@PathVariable String userId) {
+		return "Message for UserId: " + userId;
+	}
+
+}
